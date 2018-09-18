c2fca576637ced14320c2c24b28f286a24a0e26d
Carlose Gomez <carlosegomez@gmail.com>
2018-09-18 12:27:55 +0200

    split facture tests in a new class

--- tests/FactureTest.py
+++ tests/FactureTest.py
@@ -5,7 +5,7 @@ import unittest
 from facture import (Client, Product, ProductLine, Facture)
 
 
-class FactureTestCase(unittest.TestCase):
+class FactureComponentsTestCase(unittest.TestCase):
 
     def test_client(self):
         client_1 = Client.Client("Pepe", "Mojica", "34567", "44 avenida de la part, Uruguay")
@@ -25,19 +25,38 @@ class FactureTestCase(unittest.TestCase):
         product_line = ProductLine.ProductLine(product_1, 3)
         self.assertEqual(product_line.product, product_1)
         self.assertEqual(product_line.quantity, 3)
+        self.assertEqual(product_line.price, product_1.price * product_line.quantity)
 
-    def test_facture(self):
-        client_1 = Client.Client("Pepe", "Mojica", "34567", "44 avenida de la part, Uruguay")
+
+class FactureTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client_1 = Client.Client("Pepe", "Mojica", "34567", "44 avenida de la part, Uruguay")
         product_1 = Product.Product("Avion", "flight game", 45.0)
         product_2 = Product.Product("100 anos de soledad", "book", 10.0)
-        product_lines = [
+        self.product_lines = [
             ProductLine.ProductLine(product_1, 3),
             ProductLine.ProductLine(product_2, 12)
         ]
-        facture = Facture.Facture(.1, client_1, product_lines)
-        self.assertEqual(facture.tva, .1)
-        self.assertEqual(facture.product_lines, product_lines)
-        self.assertEqual(facture.client, client_1)
+        self.facture = Facture.Facture(.1, self.client_1, self.product_lines)
+
+    def test_tva_value(self):
+        self.assertEqual(self.facture.tva, .1)
+
+    def test_product_lines_content(self):
+        self.assertEqual(self.facture.product_lines, self.product_lines)
+
+    def test_client_content(self):
+        self.assertEqual(self.facture.client, self.client_1)
+
+    def test_net_price_calcule(self):
+        self.assertEqual(self.facture.net_price, 255.0)
+
+    def test_tva_calcule(self):
+        self.assertEqual(self.facture.only_tva, 25.5)
+
+    def test_total_price_calcule(self):
+        self.assertEqual(self.facture.total_price, 280.5)
 
 
 if __name__ == '__main__':
