16eb38ef7a89de431e295b1a48efc6762b43bad7
Carlose Gomez <carlosegomez@gmail.com>
2018-09-19 17:42:31 +0200

    first version with pytest

---
+++ tests/mock_pytest_demo.py
@@ -0,0 +1,94 @@
+import pytest
+import requests
+import responses
+
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    first_name: str = ''
+    last_name: str = ''
+    email: str = ''
+    user_name: str = ''
+
+    @classmethod
+    def create_from_api(cls, result):
+        record = result['results'][0]
+        return cls(record['name']['first'], record['name']['last'], record['email'], record['login']['username'])
+
+
+class APIDisconnectException(Exception):
+    pass
+
+
+class HttpNotFoundException(Exception):
+    pass
+
+
+def get_user():
+    try:
+        response = requests.get('https://randomuser.me/api/')
+
+        if response.status_code == 404:
+            raise HttpNotFoundException()
+
+        return User.create_from_api(response.json())
+    except requests.exceptions.ConnectionError:
+        raise APIDisconnectException()
+
+
+USER_MOCK = {
+    'results': [{
+        'name': dict(title='ms', first='mabel', last='fleming'),
+        'email': 'mabel.fleming@example.com',
+        'login': dict(username='heavyfrog837')
+    }]
+}
+
+
+@pytest.mark.skip
+@responses.activate
+def test_response_ok(self):
+    responses.add(responses.GET, 'https://randomuser.me/api/', json=USER_MOCK)
+    user = get_user()
+    self.assertIsInstance(user, User)
+    self.assertEqual(user.first_name, 'mabel')
+    self.assertEqual(user.last_name, 'fleming')
+
+
+@pytest.mark.skip
+@responses.activate
+def test_response_ko(self):
+    responses.add(responses.GET, 'https://randomuser.me/api/', status=404)
+    with self.assertRaises(HttpNotFoundException):
+        get_user()
+
+
+@pytest.mark.skip
+@responses.activate
+def test_connection_ko(self):
+    with self.assertRaises(APIDisconnectException):
+        get_user()
+
+
+def test(mocker):
+    mock_user = User.create_from_api(USER_MOCK)
+    mocker.patch('tests.mock_pytest_demo.get_user', return_value=mock_user)
+    user = get_user()
+    assert user.first_name == 'mabel'
+    assert  user.last_name == 'fleming'
+
+#
+# if __name__ == '__main__':
+#     pass
+    # unittest.main()
+    # try:
+    #     user = get_user('https://randomuser.me/api/')
+    #     print(user)
+    # except APIDisconnectException:
+    #     print('Connection error')
+    # except HttpNotFoundException:
+    #     print('Address not found')
+
+
+
